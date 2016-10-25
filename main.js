a9c782c34096851217d4a6f3b08e8f63cd2c14dd
xiaoheng <xiaoheng@xiaohengdeMacBook-Pro.local>
2016-10-25 11:20:11 +0800

    demo

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
-
+  
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
 
